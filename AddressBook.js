808b3feb4721320d70efa5924bc7424f1fc67b21
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-28 19:38:41 +0530

    Ability to create new Contact in Array

--- AddressBook.js
+++ AddressBook.js
@@ -7,3 +7,6 @@ const zip = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 const phoneNumber = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
 const email = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
+//UC-03---> Using array to store the new contacts.
+let addressBookArray = new Array();
+
